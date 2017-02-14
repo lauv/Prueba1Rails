5e7c3054278105f459a3324739424f6874d541f6
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-14 11:19:58 -0300

    Commit Item N° 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/batman'
-  root 'pages#batman'
+  root 'pages#superman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
