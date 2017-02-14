1d909786f84dbcdea659e94314b57ec87be9a3bd
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-14 11:59:23 -0300

    Commit Item N° 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/batman'
-  root 'pages#superman'
+  root'pages#batman-vs-superman', to: "pages#batman_vs_superman"
+  get 'pages/superman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
