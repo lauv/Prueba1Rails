a7a21dc579f7575f66fec779a26846f412319dce
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-14 12:48:42 -0300

    Commit Item N° 13

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def batman
   end
+
+  def submitb
+  	# fail params.inspect
+  	VotosBatman.create({name: params[:nameb], email: params[:emailb]})
+  	redirect_to(pages_batman_path, notice: "Voto para Batman was saved")
+  end
+  
+  def submits
+  	# fail params.inspect
+  	VotosSuperman.create({name: params[:names], email: params[:emails]})
+  	redirect_to(pages_superman_path, notice: "Voto para Superman was saved")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pages/batman'
   root'pages#batman-vs-superman', to: "pages#batman_vs_superman"
   get 'pages/superman'
+  post 'pages/submitb'
+  post 'pages/submits'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
